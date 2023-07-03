9b9866afc3a49b2557dcb9e425e1d8b741a13e39
misko <misko@cs.toronto.edu>
2023-07-03 09:07:26 +0000

    change to gelu

--- software/model_training_and_inference/models/models.py
+++ software/model_training_and_inference/models/models.py
@@ -100,11 +100,12 @@ class TransformerModel(nn.Module):
 			n_heads, 
 			d_hid, 
 			dropout,
+                        activation='gelu',
 			batch_first=True)
 		self.transformer_encoder = TransformerEncoder(
 			encoder_layers, 
 			n_layers,
-			nn.LayerNorm(d_model),
+			#nn.LayerNorm(d_model),
 			)
 		
 		assert( d_model>d_radio_feature)
