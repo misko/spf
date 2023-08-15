ba57ccf7d379e9daebde169717c1be6e9f032715
Misko <misko@cs.toronto.edu>
2023-08-15 16:13:20 -0700

    fixes , forgot to set sample rate on tx

--- hardware/SDR_plutoplus/test_emitter_recv/03_only_emit.py
+++ hardware/SDR_plutoplus/test_emitter_recv/03_only_emit.py
@@ -4,10 +4,16 @@ import adi
 import numpy as np
 from math import lcm
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", type=str, help="target Pluto IP address",required=True)
+args = parser.parse_args()
+
 c=3e8
-fc0 = int(4000)
+fc0 = int(1e5)
 fs = int(4e6)    # must be <=30.72 MHz if both channels are enabled
-rx_lo = int(2.45e9) #4e9
+rx_lo = int(2.5e9) #4e9
 tx_lo = rx_lo
 
 rx_mode = "manual"  # can be "manual" or "slow_attack"
@@ -18,22 +24,25 @@ wavelength = c/rx_lo              # wavelength of the RF carrier
 
 rx_n=int(2**10)
 
-sdr = adi.ad9361(uri='ip:192.168.3.2')
+sdr = adi.ad9361(uri='ip:%s' % args.ip)
 
 #setup TX
+sdr.sample_rate=fs
 sdr.tx_enabled_channels = [0]
-sdr.tx_rf_bandwidth = int(fc0)
+sdr.tx_rf_bandwidth = int(3*fc0)
 sdr.tx_lo = int(tx_lo)
 sdr.tx_cyclic_buffer = True # this keeps repeating!
-sdr.tx_hardwaregain_chan0 = int(-10) #tx_gain) #tx_gain)
+sdr.tx_hardwaregain_chan0 = int(-5) #tx_gain) #tx_gain)
 sdr.tx_hardwaregain_chan1 = int(-80) # use Tx2 for calibration
 #
 tx_n=int(min(lcm(fc0,fs),rx_n*8)) #1024*1024*1024) # tx for longer than rx
+tx_n=int(lcm(fc0,fs))
 sdr.tx_buffer_size = tx_n
+print(sdr.tx_lo,tx_n,lcm(fc0,fs))
 
 #since its a cyclic buffer its important to end on a full phase
-t = np.arange(0, tx_n)/fs
-iq0 = np.exp(1j*2*np.pi*t*fc0)*(2**14)
+t = np.arange(0, tx_n)/fs # time at each point assuming we are sending samples at (1/fs)s
+iq0 = np.exp(1j*2*np.pi*fc0*t)*(2**14)
 sdr.tx(iq0)  # Send Tx data.
 
 import time
--- hardware/SDR_plutoplus/test_emitter_recv/04_plot_signal.py
+++ hardware/SDR_plutoplus/test_emitter_recv/04_plot_signal.py
@@ -6,28 +6,33 @@ import numpy as np
 from math import lcm
 #from sdr import *
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", type=str, help="target Pluto IP address",required=True)
+args = parser.parse_args()
 
 c=3e8
-fc0 = int(500e3)
+fc0 = int(1e5)
 fs = int(4e6)    # must be <=30.72 MHz if both channels are enabled
-rx_lo = int(2.45e9) #4e9
+rx_lo = int(2.5e9) #4e9
 tx_lo = rx_lo
 
 rx_mode = "manual"  # can be "manual" or "slow_attack"
-rx_gain = -30 
+rx_gain = -10 
 tx_gain = -30
 
 wavelength = c/rx_lo              # wavelength of the RF carrier
 
 rx_n=int(2**14)
 
-sdr = adi.ad9361(uri='ip:192.168.3.1')
+sdr = adi.ad9361(uri='ip:%s' % args.ip)
 
 #setup RX
 sdr.rx_enabled_channels = [0, 1]
 sdr.sample_rate = fs
 assert(sdr.sample_rate==fs)
-sdr.rx_rf_bandwidth = int(fc0*3)
+sdr.rx_rf_bandwidth = int(fs) #fc0*5)
 sdr.rx_lo = int(rx_lo)
 sdr.gain_control_mode = rx_mode
 sdr.rx_hardwaregain_chan0 = int(rx_gain)
@@ -37,21 +42,36 @@ sdr._rxadc.set_kernel_buffers_count(1)   # set buffers to 1 (instead of the defa
 
 #setup TX
 sdr.tx_enabled_channels = []
-sdr.tx_rf_bandwidth = int(fc0*3)
-sdr.tx_lo = int(tx_lo)
-sdr.tx_cyclic_buffer = True # this keeps repeating!
-sdr.tx_hardwaregain_chan0 = int(-80) #tx_gain) #tx_gain)
-sdr.tx_hardwaregain_chan1 = int(-80) # use Tx2 for calibration
+#sdr.tx_rf_bandwidth = int(fc0*3)
+#sdr.tx_lo = int(tx_lo)
+#sdr.tx_cyclic_buffer = True # this keeps repeating!
+#sdr.tx_hardwaregain_chan0 = int(-80) #tx_gain) #tx_gain)
+#sdr.tx_hardwaregain_chan1 = int(-80) # use Tx2 for calibration
 
 import time
 fig,axs=plt.subplots(1,1,figsize=(4,4))
 
+
+
+
+import matplotlib.pyplot as plt
+
+fig,axs=plt.subplots(2,2,figsize=(10,8))
+
 t=np.arange(rx_n)
 while True:
 	signal_matrix=np.vstack(sdr.rx())
-	plt.clf()
-	plt.ylim([-2500,2500])
-	plt.scatter(t,signal_matrix[0].real,s=1)
-	#plt.plot(t,signal_matrix[1].real)
-	plt.draw()
+
+	freq = np.fft.fftfreq(t.shape[-1],d=1.0/fs)
+	assert(t.shape[-1]==rx_n)
+	for idx in [0,1]:
+		axs[idx][0].clear()
+		axs[idx][1].clear()
+		axs[idx][0].scatter(t,signal_matrix[idx].real,s=1)
+		sp = np.fft.fft(signal_matrix[idx])
+		#sp[:int(rx_n/8)]=0
+		#sp[-int(rx_n/8):]=0
+		axs[idx][1].scatter(freq, sp.real,s=1) #, freq, sp.imag)
+		print("MAXFREQ",freq[np.abs(np.argmax(sp.real))])
+	fig.canvas.draw()
 	plt.pause(0.00001)
