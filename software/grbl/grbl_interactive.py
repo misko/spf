1c2457abde75835f67c1874a33ee03de5987b304
misko <misko@cs.toronto.edu>
2023-08-21 19:52:51 +0100

    fix wall physics

--- software/grbl/grbl_interactive.py
+++ software/grbl/grbl_interactive.py
@@ -2,11 +2,7 @@ import serial
 import time
 import sys
 import numpy as np
-if len(sys.argv)!=2:
-        print("%s device" % sys.argv[0])
-        sys.exit(1)
-
-serial_fn=sys.argv[1]
+import matplotlib.pyplot as plt
 
 # a is the left motor on the wall
 # b is the right motor on the wall
@@ -27,7 +23,6 @@ def from_steps(a_motor_steps,b_motor_steps):
 
     xy_g=origin_a-y_dir*y+x_dir*x
     return xy_g
-
 
 def to_steps(p):
     #limits (0,1500)  and (3180-300,4000)
@@ -58,7 +53,7 @@ def spiral():
         print(x,y)
         p=np.array([x,y])+center
         a_motor_steps,b_motor_steps=to_steps(p)
-        cmd="G0 X%0.2f Y%0.2f" % (a_motor_steps,b_motor_steps)
+        cmd="G0 X%0.2f Y%0.2f" % (b_motor_steps,a_motor_steps)
         #print("SENDING",x,y,cmd)
         s.write((cmd + '\n').encode()) # Send g-code block to grbl
         print(s.readline().strip())
@@ -72,59 +67,152 @@ def calibrate():
             s.write((cmd + '\n').encode()) # Send g-code block to grbl
 
 
-# Open grbl serial port ==> CHANGE THIS BELOW TO MATCH YOUR USB LOCATION
-s = serial.Serial(serial_fn,115200) # GRBL operates at 115200 baud. Leave that part alone.
-
-s.write("?".encode())
-print(s.readline().strip())
-s.write("?".encode())
-print(s.readline().strip())
+def open_grbl(serial_fn):
+    # Open grbl serial port ==> CHANGE THIS BELOW TO MATCH YOUR USB LOCATION
+    s = serial.Serial(serial_fn,115200) # GRBL operates at 115200 baud. Leave that part alone.
+    s.write("?".encode())
+    grbl_out=s.readline() # get the response
+    print("GRBL ONLINE",grbl_out)
+    time.sleep(0.05)
+    return s
 
 def push_reset(s):
     s.write(b'\x18')
     return s.readline().decode().strip()
 
 def get_status(s):
+    time.sleep(0.1)
     s.write("?".encode())
+    time.sleep(0.1)
     response=s.readline().decode().strip()
+    time.sleep(0.1)
+    print("STATUS",response)
     #<Idle|MPos:-3589.880,79.560,0.000,0.000|FS:0,0>
-    motor_position_str=response.split("|")[1]
-    xmotor,ymotor,
-    return s.readline().decode().strip()
+    try:
+        motor_position_str=response.split("|")[1]
+    except:
+        print("FAILED TO PARSE",response)
+    b_motor_steps,a_motor_steps,_,_=map(float,motor_position_str[len('MPos:'):].split(','))
+    time.sleep(0.01)
+    return a_motor_steps,b_motor_steps,from_steps(a_motor_steps,b_motor_steps)
 
+def wait_while_moving(s):
+    while True:
+        a,b,xy=get_status(s)
+        time.sleep(0.1)
+        _a,_b,_xy=get_status(s)
+        if _a==a and _b==b:
+            return 
+        print(a,b)
+        time.sleep(0.1)
 
-#sys.exit(1)
-#time.sleep(2)    # Wait for grbl to initialize
-#s.flushInput()  # Flush startup text in serial input
-for line in sys.stdin:
-    line=line.strip()
-    if line=='q':
-        sys.exit(1)
-    elif line=='r':
-        r=push_reset(s)
-        print(r)
-    elif line=='s':
-        r=get_status(s)
-        #<Idle|MPos:-3589.880,79.560,0.000,0.000|FS:0,0>
-        print(r)
-    elif line=='c':
-        calibrate()
-    elif line=='e':
-        spiral()
+def binary_search_edge(left, right, xy, direction, epsilon):
+    if (right-left)<epsilon:
+        return left
+    l=(right+left)/2
+    p=l*direction+xy
+    steps=to_steps(np.copy(p))
+    actual=from_steps(*steps)
+    deviation=np.linalg.norm(p-actual)
+    if deviation>0.0001:
+        #go back left
+        return binary_search_edge(left,l,xy, direction, epsilon)
+    return binary_search_edge(l,right,xy,direction,epsilon)
+
+def full_field_step(s,direction):
+    #find out at what point xy+l*direction we stop changing one of the variables
+    l=binary_search_edge(0,10000,xy,direction,0.01)
+    #from_steps(*to_steps(l*direction+s))
+
+def bounce(s,direction,xy=None):
+    #find current position
+    #a_motor_steps,b_motor_steps,xy=get_status(s)
+    #pick a random direction
+    #take full field step 
+    #if hit a wall 
+    #direction=np.array([1,0])
+    #direction=direction/np.linalg.norm(direction)
+    if xy is None:
+        a_motor_steps,b_motor_steps,xy=get_status(s)
+    #find out at what point xy+l*direction we stop changing one of the variables
+    epsilon=0.001
+    l=binary_search_edge(0,10000,xy,direction,epsilon)
+    #find a paralell vector to the boundary
+    p1=from_steps(*to_steps((l+2*epsilon)*direction+xy))
+    p2=from_steps(*to_steps((l+3*epsilon)*direction+xy))
+    if np.linalg.norm(p1-p2)<epsilon**2: # the direction is only X or Y
+        new_direction=-direction
     else:
-        if True:
-            p=np.array([ float(x) for x in line.split() ])
-            a_motor_steps,b_motor_steps=to_steps(p)
-            cmd="G0 X%0.2f Y%0.2f" % (b_motor_steps,a_motor_steps)
-            print("SENDING",cmd)
-            print(from_steps(a_motor_steps,b_motor_steps))
-            s.write((cmd + '\n').encode()) # Send g-code block to grbl
-            grbl_out = s.readline() # Wait for grbl response with carriage return
-            print(grbl_out)
-        #except:
-        #    print("ERROR IN INPUT")
+        b=p2-p1
+        b/=np.linalg.norm(b)
+        bn=np.array([-b[1],b[0]])
+        _xy=from_steps(*to_steps(xy))
+        if np.linalg.norm(from_steps(*to_steps(_xy+bn))-_xy)<epsilon:
+            bn=-bn
+        new_direction=np.dot(direction,b)*b-np.dot(direction,bn)*bn
+    theta=np.random.uniform(2*np.pi)
+    #new_direction=0.95*new_direction+0.05*np.array([np.sin(theta),np.cos(theta)])
+    jiggle=0#np.random.uniform(-2,2,xy.shape) 
+    return l*direction+xy+jiggle,new_direction
 
+def move_to(s,p):
+    a_motor_steps,b_motor_steps=to_steps(p)
+    cmd="G0 X%0.2f Y%0.2f" % (b_motor_steps,a_motor_steps)
+    time.sleep(0.1)
+    s.write((cmd + '\n').encode()) # Send g-code block to grbl
+    time.sleep(0.1)
+    grbl_out = s.readline() # Wait for grbl response with carriage return
+    print("MOVE TO OUT",grbl_out)
+    time.sleep(0.1)
 
-# Close file and serial port
-f.close()
-s.close()
+if __name__=='__main__':
+    if len(sys.argv)!=2:
+            print("%s device" % sys.argv[0])
+            sys.exit(1)
+
+    serial_fn=sys.argv[1]
+
+    s=open_grbl(serial_fn)
+    for line in sys.stdin:
+        line=line.strip()
+        if line=='q':
+            sys.exit(1)
+        elif line=='r':
+            r=push_reset(s)
+            print(r)
+        elif line=='f':
+            theta=np.random.uniform(2*np.pi)
+            direction=np.array([np.sin(theta),np.cos(theta)])
+
+            #point=np.array([2491.49001749,2401.75483327])
+            #direction=np.array([0.63471637,0.57157117])
+            #print("Point",point,direction)
+            for _ in range(30):
+                point,direction=bounce(s,direction)
+                print(time.time(),"TO",point,"new direction",direction)
+                print("")
+                move_to(s,point)
+                wait_while_moving(s)
+                 
+            #full_field_step(s,np.array([1,0]))
+        elif line=='s':
+            r=get_status(s)
+            #<Idle|MPos:-3589.880,79.560,0.000,0.000|FS:0,0>
+            print(r)
+        elif line=='c':
+            calibrate()
+        elif line=='e':
+            spiral()
+        else:
+            if True:
+                p_main=np.array([ float(x) for x in line.split() ])
+                a_motor_steps,b_motor_steps=to_steps(p_main)
+                cmd="G0 X%0.2f Y%0.2f" % (b_motor_steps,a_motor_steps)
+                print("SENDING",cmd)
+                print(from_steps(a_motor_steps,b_motor_steps))
+                s.write((cmd + '\n').encode()) # Send g-code block to grbl
+                grbl_out = s.readline() # Wait for grbl response with carriage return
+                print(grbl_out)
+        time.sleep(0.1)
+
+    s.close()
