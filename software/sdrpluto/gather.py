8cd7b84753a9412ff183177edf0357f56ae34b09
Misko <misko@cs.toronto.edu>
2023-09-11 20:15:06 -0700

    gather working with phase calibration on recv

--- software/sdrpluto/gather.py
+++ software/sdrpluto/gather.py
@@ -5,7 +5,95 @@ from math import gcd
 import matplotlib.pyplot as plt
 c=3e8
 
-def setup(args):
+def setup_rxtx_and_phase_calibration(args):
+    fc0 = int(args.fi)
+    fs = int(args.fs)    # must be <=30.72 MHz if both channels are enabled
+    rx_lo = int(args.fc) #4e9
+    tx_lo = rx_lo
+
+    # setup receive
+    rx_mode = "slow_attack"  # can be "manual" or "slow_attack"
+    rx_gain = 40 
+    tx_gain = -30
+
+    rx_n=int(2**8)
+
+
+    retries=0
+    while retries<10:
+        #try to setup TX and lets see if it works
+        #sdr_emitter = adi.ad9361(uri='ip:%s' % args.emitter_ip)
+        sdr_rxtx = adi.ad9361(uri='ip:%s' % args.receiver_ip)
+
+        sdr_rxtx.rx_enabled_channels = [0, 1]
+        sdr_rxtx.sample_rate = fs
+        assert(sdr_rxtx.sample_rate==fs)
+        sdr_rxtx.rx_rf_bandwidth = int(fs) #fc0*5) #TODO!
+        sdr_rxtx.rx_lo = int(rx_lo)
+        sdr_rxtx.gain_control_mode = rx_mode
+        sdr_rxtx.rx_hardwaregain_chan0 = int(rx_gain)
+        sdr_rxtx.rx_hardwaregain_chan1 = int(rx_gain)
+        sdr_rxtx.rx_buffer_size = int(rx_n)
+        sdr_rxtx._rxadc.set_kernel_buffers_count(1)   # set buffers to 1 (instead of the default 4) to avoid stale data on Pluto
+
+        #drop the first bunch of frames
+        for _ in range(20):
+            sdr_rxtx.rx()
+
+        #setup TX
+        sdr_rxtx.tx_rf_bandwidth = int(fs)
+        assert(sdr_rxtx.tx_rf_bandwidth==int(fs))
+        sdr_rxtx.tx_lo = int(tx_lo)
+        assert(sdr_rxtx.tx_lo==tx_lo)
+        sdr_rxtx.tx_enabled_channels = [1]
+        sdr_rxtx.tx_hardwaregain_chan0 = int(-80) #tx_gain) #tx_gain)
+        sdr_rxtx.tx_hardwaregain_chan1 = int(-5) # use Tx2 for calibration
+        assert(sdr_rxtx.tx_hardwaregain_chan1==int(-5))
+        #
+        tx_n=int(fs/gcd(fs,fc0))
+        while tx_n<1024*16:
+            tx_n*=2
+        #sdr_rxtx.tx_buffer_size = tx_n
+
+        #since its a cyclic buffer its important to end on a full phase
+        t = np.arange(0, tx_n)/fs # time at each point assuming we are sending samples at (1/fs)s
+        iq0 = np.exp(1j*2*np.pi*fc0*t)*(2**14)
+        #try to reset the tx
+        #sdr_rxtx.tx_destroy_buffer()
+        sdr_rxtx.tx_cyclic_buffer = True # this keeps repeating!
+        assert(sdr_rxtx.tx_cyclic_buffer==True)
+        sdr_rxtx.tx(iq0)  # Send Tx data.
+        
+        #give RX a chance to calm down
+        for _ in range(50):
+            sdr_rxtx.rx()
+
+        #test to see what frequency we are seeing
+        freq = np.fft.fftfreq(rx_n,d=1.0/fs)
+        signal_matrix=np.vstack(sdr_rxtx.rx())
+        sp = np.fft.fft(signal_matrix[0])
+        max_freq=freq[np.abs(np.argmax(sp.real))]
+        if np.abs(max_freq-args.fi)<(args.fs/rx_n+1):
+            print("TX ONLINE!")
+            break
+        retries+=1
+
+    #get some new data
+    for retry in range(20):
+      n_calibration_frames=200
+      phase_calibrations=np.zeros(n_calibration_frames)
+      for idx in range(n_calibration_frames):
+          sdr_rxtx.rx()
+          phase_calibrations[idx]=((np.angle(signal_matrix[0])-np.angle(signal_matrix[1]))%(2*np.pi)).mean() # TODO THIS BREAKS if diff is near 2*np.pi...
+      if phase_calibrations.std()<1e-5:
+        print("FINAL PHASE CALIBRATION",phase_calibrations.mean(),phase_calibrations.mean()/(2*np.pi))
+        sdr_rxtx.phase_calibration=phase_calibrations.mean()
+        return sdr_rxtx
+    return None
+
+
+
+def setup_rx_and_tx(args):
     fc0 = int(args.fi)
     fs = int(args.fs)    # must be <=30.72 MHz if both channels are enabled
     rx_lo = int(args.fc) #4e9
@@ -92,6 +180,7 @@ def plot_recv_signal(sdr_rx):
     t=np.arange(rx_n)
     while True:
       signal_matrix=np.vstack(sdr_rx.rx())
+      signal_matrix[1]*=np.exp(1j*sdr_rx.phase_calibration)
 
       freq = np.fft.fftfreq(t.shape[-1],d=1.0/sdr_rx.sample_rate)
       assert(t.shape[-1]==rx_n)
@@ -134,7 +223,14 @@ if __name__=='__main__':
     parser.add_argument("--rx-gain", type=int, help="RX gain",required=False,default=40)
     parser.add_argument("--tx-gain", type=int, help="TX gain",required=False,default=-3)
     args = parser.parse_args()
-    sdr_rx,sdr_tx=setup(args)
+
+    #calibrate the receiver
+    sdr_rx=setup_rxtx_and_phase_calibration(args)
+    phase_calibration=sdr_rx.phase_calibration
+    sdr_rx=None
+    sdr_rx,sdr_tx=setup_rx_and_tx(args)
+    #apply the previous calibration
+    sdr_rx.phase_calibration=phase_calibration
 
 
     plot_recv_signal(sdr_rx)
