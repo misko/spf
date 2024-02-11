a628796ec2acc3666dc132b553de3bb78730de6a
Misko <misko@cs.toronto.edu>
2024-02-11 11:39:08 -0800

    esp32 FCC testing

---
+++ spf/radio_targets/wifi/arduino_companion/tx/tx.ino
@@ -0,0 +1,14 @@
+  void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+   Serial1.println("cbw40m_en 0");
+   Serial1.println("tx_contin_en 1");
+   Serial1.println("esp_tx 1 0 0");
+   digitalWrite(LED_BUILTIN, LOW);  // turn the LED on (HIGH is the voltage level)
+   delay(120000);  
+}
+
+void loop() {
+}
