83c1ecff562f6866c53b07ab12c140aafcfaf0ba
Misko <misko@cs.toronto.edu>
2024-02-14 17:48:08 -0800

    add tx packet options for esp32

---
+++ spf/radio_targets/wifi/arduino_companion_txpacket50/arduino_companion_txpacket50.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  Serial1.println("cmdstop");
+  Serial1.println("cbw40m_en 0");
+  Serial1.println("tx_contin_en 0");
+  Serial1.println("tx_cbw40m_en 0");
+  Serial1.println("RFChannelSel 12 0");
+  Serial1.println("cmdstop");
+  Serial1.println("FillTxPacket 655410 4 0 0 0 0 1 2 3 4 5 6");
+  Serial1.println("target_power_backoff 300");
+  Serial1.println("WifiTxStart 655360 0 554 0 0 1 0");
+}
+
+void loop() {
+   digitalWrite(LED_BUILTIN, LOW);  // turn the LED on (HIGH is the voltage level)
+   delay(2000);  
+   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED  on (HIGH is the voltage level)
+   delay(1000);  
+}
