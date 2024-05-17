445b86a6f77d4d1cff3f30f849438f4823de25b0
Misko <misko@cs.toronto.edu>
2024-05-17 08:29:30 -0700

    update lambda scsript

--- spf/notebooks/run_lambda.sh
+++ spf/notebooks/run_lambda.sh
@@ -8,15 +8,15 @@ depths="2 5 8"
 hiddens="16 32 64"
 head_starts="0 1000 4000"
 seg_nets="unet conv"
+for type in $types; do 
+for lr in $lrs; do 
+for seg_net in ${seg_nets}; do
+for depth in $depths; do
+for hidden in $hiddens; do
 for bn in $bns; do
 for symmetry in $symmetrys; do 
 for act in $acts; do 
-for type in $types; do 
-for lr in $lrs; do 
 for other in $others; do
-for depth in $depths; do
-for hidden in $hiddens; do
-for seg_net in ${seg_nets}; do
 #for start in ${head_starts}; do
 bn_flag="--batch-norm" 
 if [ $bn ==  "x" ]; then
@@ -26,17 +26,17 @@ sym="--symmetry"
 if [ "$symmetry" == "x" ]; then
   sym=""
 fi
-other="--other"
+oth="--other"
 if [ "$other" == "x" ]; then
-  other=""
+  oth=""
 fi
 if [ $depth -ne 2 -a "${seg_net}" == "unet" ]; then
   continue
 fi
 python simple_train.py  -d ~/data/*.zarr  --device cuda \
  --batch 64 --workers 28 --type $type --lr $lr \
- --segmentation-level downsampled --epochs 5 --depth ${depth} \
- --hidden ${hidden} --act $act $other $sym  \
+ --segmentation-level downsampled --epochs 3 --depth ${depth} \
+ --hidden ${hidden} --act $act $oth $sym  \
  --seg-net ${seg_net} ${bn_flag} --skip-segmentation #--seg-start ${start}
  done
  done
