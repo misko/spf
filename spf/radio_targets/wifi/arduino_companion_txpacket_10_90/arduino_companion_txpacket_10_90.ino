e46ef046c82e7db4b0bff50e6fa737a19de1d4ef
Misko <misko@cs.toronto.edu>
2024-06-02 13:17:32 -0700

    updates to arduino esp32 blaster

--- spf/radio_targets/wifi/arduino_companion_txpacket_10_90/arduino_companion_txpacket_10_90.ino
+++ spf/radio_targets/wifi/arduino_companion_txpacket_10_90/arduino_companion_txpacket_10_90.ino
@@ -12,12 +12,17 @@ void set_tx(int load, int attenuation) {
   Serial1.println("RFChannelSel 1 0");
   Serial1.println("cmdstop");
   Serial1.println("FillTxPacket 655410 4 0 0 0 0 1 2 3 4 5 6");
-  Serial1.print("target_power_backoff");
+  Serial1.print("target_power_backoff ");
   Serial1.println(attenuation);
   if (load==90) { 
-    Serial1.println("WifiTxStart 655360 0 67 0 0 1 0"); // 90 -> 67 , 10 -> 5354 , 50 -> 554
+    //Serial1.println("WifiTxStart 655360 0 67 0 0 1 0"); // 90 -> 67 , 10 -> 5354 , 50 -> 554
+    Serial1.println("WifiTxStart 655360 0 323 0 0 1 0"); // 90 -> 67 , 10 -> 5354 , 50 -> 554
   } else if (load==50) {
     Serial1.println("WifiTxStart 655360 0 554 0 0 1 0"); // 90 -> 67 , 10 -> 5354 , 50 -> 554
+  } else if (load==35) {
+    Serial1.println("WifiTxStart 655360 0 1527 0 0 1 0"); // 90 -> 67 , 10 -> 5354 , 50 -> 554
+  } else if (load==25) {
+    Serial1.println("WifiTxStart 655360 0 2500 0 0 1 0"); // 90 -> 67 , 10 -> 5354 , 50 -> 554
   } else if (load==10) {
     Serial1.println("WifiTxStart 655360 0 5354 0 0 1 0"); // 90 -> 67 , 10 -> 5354 , 50 -> 554
   } else {
@@ -27,18 +32,28 @@ void set_tx(int load, int attenuation) {
 }
 
 void loop() {
-   long load = random(0, 2); // load
+   long load = random(0, 4); // load
    if (load==0) {
     load=10;
    } else if (load==1) {
     load=50;
-   } else { //else if (load==2) {
+   } else if (load==2) {
+    load=25;
+   } else if (load==3) {
+    load=35;
+   } else { //else if (load==3) {
     load=90;
    }
-   long attenuation = random(30, 100); /// attenuation  
+   long attenuation = 50; // random(30, 30); /// attenuation  
    set_tx(load, attenuation);
-   digitalWrite(LED_BUILTIN, LOW);  // turn the LED on
-   delay(1000*120); // two minutes
+   for (int idx=0; idx<30; idx++) { // 30 seconds in each state
+    if (idx%5==0) {
+      digitalWrite(LED_BUILTIN, LOW);  // turn the LED on
+    } else {
+      digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on
+    }
+    delay(1000); 
+   }
    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED  off
    delay(1000);  
 }
