27fbf8c91715af426b023c4c0b2fac7826fc3cd1
misko <misko@cs.toronto.edu>
2024-08-01 02:36:31 +0100

    new capture script

--- data_collection_model_and_results/2d_wall_array/2d_wall_array_v2/run_capture.sh
+++ data_collection_model_and_results/2d_wall_array/2d_wall_array_v2/run_capture.sh
@@ -6,14 +6,30 @@ reboot_plutos () {
     done
 }
 
+
 export ROOT="../../../"
+config=${ROOT}/spf/v5_configs/wall_array_v2_external_50p75.yaml
 
 reboot_plutos
-python ${ROOT}/spf/grbl_radio_collection.py  -c ${ROOT}/spf/v5_configs/wall_array_v2_external.yaml -r rx_circle -s /dev/ttyACM0
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r bounce -s /dev/ttyACM0 --dry-run --n-records 1
+
+
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r rx_circle -s /dev/ttyACM0 --n-records 5000
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r bounce -s /dev/ttyACM0 --dry-run --n-records 1
+
+reboot_plutos 
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r bounce -s /dev/ttyACM0 --dry-run --n-records 1
+
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r bounce -s /dev/ttyACM0 --n-records 10000
+
 reboot_plutos
-python ${ROOT}/spf/grbl_radio_collection.py  -c ${ROOT}/spf/v5_configs/wall_array_v2_external.yaml -r bounce -s /dev/ttyACM0
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r bounce -s /dev/ttyACM0 --dry-run --n-records 1
+
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r bounce -s /dev/ttyACM0 --n-records 10000
+
 reboot_plutos
-python ${ROOT}/spf/grbl_radio_collection.py  -c ${ROOT}/spf/v5_configs/wall_array_v2_external.yaml -r bounce -s /dev/ttyACM0
-reboot_plutos
-python ${ROOT}/spf/grbl_radio_collection.py  -c ${ROOT}/spf/v5_configs/wall_array_v2_external.yaml -r bounce -s /dev/ttyACM0
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r bounce -s /dev/ttyACM0 --dry-run --n-records 1
+
+python ${ROOT}/spf/grbl_radio_collection.py  -c ${config} -r bounce -s /dev/ttyACM0 --n-records 10000
+
 cat ./go_home | python ${ROOT}/spf/grbl/grbl_interactive.py /dev/ttyACM0
