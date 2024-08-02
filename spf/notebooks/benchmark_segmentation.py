f51b64079567b7a0d7e7c394d9ee0d55f9c7bcdf
misko <misko@cs.toronto.edu>
2024-08-02 01:34:07 +0000

    add script to test

---
+++ spf/notebooks/benchmark_segmentation.py
@@ -0,0 +1,67 @@
+import argparse
+import tempfile
+
+from spf.dataset.spf_dataset import mp_segment_zarr, v5spfdataset
+from spf.rf import precompute_steering_vectors
+
+
+def get_parser_filter():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        help="dataset prefixes",
+        required=True,
+    )
+    parser.add_argument(
+        "--precompute-cache",
+        type=str,
+        help="precompute cache",
+        required=True,
+    )
+    parser.add_argument(
+        "--nthetas",
+        type=int,
+        default=65,
+        required=False,
+    )
+    return parser
+
+
+# from pyinstrument.renderers import ConsoleRenderer
+
+
+def segment(args):
+    ds = v5spfdataset(
+        args.dataset,
+        precompute_cache=args.precompute_cache,
+        nthetas=args.nthetas,
+        skip_signal_matrix=True,
+        paired=True,
+        ignore_qc=True,
+        gpu=False,
+        snapshots_per_session=1,
+        readahead=False,
+        skip_simple_segmentations=False,
+    )
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        print("SEG")
+        mp_segment_zarr(
+            args.dataset,
+            str(tmpdirname) + "/tmp",
+            ds.steering_vectors,
+            precompute_to_idx=-1,
+            gpu=False,
+            n_parallel=0,
+            skip_beamformer=False,
+        )
+
+
+if __name__ == "__main__":
+    parser = get_parser_filter()
+    args = parser.parse_args()
+    # with Profile() as profile:
+    # profiler = Profiler()
+    # profiler.start()
+    segment(args)
