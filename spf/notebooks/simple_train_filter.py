376bc285c1a1b49acab0e5c332c66382041353b6
misko <misko@cs.toronto.edu>
2024-09-14 21:54:47 +0000

    enable isort

--- spf/notebooks/simple_train_filter.py
+++ spf/notebooks/simple_train_filter.py
@@ -1,75 +1,43 @@
 import argparse
-from functools import cache, partial
+import gc
 import math
+import random
+from cProfile import Profile
+from functools import cache, partial
+from math import ceil
+from pstats import SortKey, Stats
+from random import shuffle
 
 import numpy as np
 import tensordict
 import torch
 from matplotlib import pyplot as plt
+from torch.nn import LayerNorm, TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import BatchSampler, DistributedSampler, Sampler
 from tqdm import tqdm
-from random import shuffle
+
 import wandb
-import math
 from spf.dataset.spf_dataset import (
     v5_collate_beamsegnet,
     v5_collate_keys_fast,
     v5spfdataset,
 )
-from math import ceil
 from spf.model_training_and_inference.models.beamsegnet import (
-    BeamNetDiscrete,
     BeamNetDirect,
+    BeamNetDiscrete,
     SimpleNet,
 )
-import gc
-from cProfile import Profile
-from pstats import SortKey, Stats
-
 from spf.rf import (
     reduce_theta_to_positive_y,
     torch_pi_norm,
     torch_pi_norm_pi,
     torch_reduce_theta_to_positive_y,
 )
-
-import random
-
-from torch.nn import (
-    TransformerEncoder,
-    TransformerEncoderLayer,
-    LayerNorm,
-)
-
-
-from torch.utils.data import DistributedSampler, Sampler, BatchSampler
+from spf.utils import PositionalEncoding, StatefulBatchsampler
 
 torch.set_float32_matmul_precision("high")
 
 DIST_NORM = 4000
-
-
-class PositionalEncoding(torch.nn.Module):
-
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
-        super().__init__()
-        self.dropout = torch.nn.Dropout(p=dropout)
-
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
-        )
-        pe = torch.zeros(1, max_len, d_model)
-        pe[0, :, 0::2] = torch.sin(position * div_term)
-        pe[0, :, 1::2] = torch.cos(position * div_term)
-        self.register_buffer("pe", pe)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Arguments:
-            x: Tensor, shape ``[batch_size, seq_len, embedding_dim]``
-        """
-        x = x + self.pe[:, : x.size(1)]
-        return self.dropout(x)
 
 
 # torch.autograd.set_detect_anomaly(True)
