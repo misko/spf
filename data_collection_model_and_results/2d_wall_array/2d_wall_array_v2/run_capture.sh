896ccc8d6fd47e8f53947cb5a4650aac53ed8534
misko <misko@cs.toronto.edu>
2024-10-20 22:56:40 +0100

    use new capture with LoRa

--- data_collection_model_and_results/2d_wall_array/2d_wall_array_v2/run_capture.sh
+++ data_collection_model_and_results/2d_wall_array/2d_wall_array_v2/run_capture.sh
@@ -8,7 +8,7 @@ reboot_plutos () {
 
 
 export ROOT="../../../"
-config=${ROOT}/spf/v5_configs/wall_array_v2_external_35.yaml
+config=${ROOT}/spf/v5_configs/wall_array_v2_external_75_838M.yaml
 
 #reboot_plutos
 sleep 3
