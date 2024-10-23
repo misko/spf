d2d99ad012ba51a5ee1cc90de5f097aa52234dee
misko <misko@cs.toronto.edu>
2024-10-23 04:43:50 +0000

    add parallel to segmenting

--- spf/dataset/spf_dataset.py
+++ spf/dataset/spf_dataset.py
@@ -18,59 +18,28 @@ from tensordict import TensorDict
 from torch.utils.data import Dataset
 
 from spf.dataset.rover_idxs import (  # v3rx_column_names,
-    v3rx_avg_phase_diff_idxs,
-    v3rx_beamformer_start_idx,
-    v3rx_column_names,
-    v3rx_gain_idxs,
-    v3rx_rssi_idxs,
-    v3rx_rx_pos_idxs,
-    v3rx_rx_theta_idx,
-    v3rx_time_idxs,
-)
+    v3rx_avg_phase_diff_idxs, v3rx_beamformer_start_idx, v3rx_column_names,
+    v3rx_gain_idxs, v3rx_rssi_idxs, v3rx_rx_pos_idxs, v3rx_rx_theta_idx,
+    v3rx_time_idxs)
 from spf.dataset.spf_generate import generate_session
 from spf.dataset.v5_data import v5rx_2xf64_keys, v5rx_f64_keys
-from spf.dataset.wall_array_v1_idxs import (
-    v1_beamformer_start_idx,
-    v1_column_names,
-    v1_time_idx,
-    v1_tx_pos_idxs,
-)
+from spf.dataset.wall_array_v1_idxs import (v1_beamformer_start_idx,
+                                            v1_column_names, v1_time_idx,
+                                            v1_tx_pos_idxs)
 from spf.dataset.wall_array_v2_idxs import (  # v3rx_column_names,
-    v2_avg_phase_diff_idxs,
-    v2_beamformer_start_idx,
-    v2_column_names,
-    v2_gain_idxs,
-    v2_rssi_idxs,
-    v2_rx_pos_idxs,
-    v2_rx_theta_idx,
-    v2_time_idx,
-    v2_tx_pos_idxs,
-)
-from spf.plot.image_utils import (
-    detector_positions_to_theta_grid,
-    labels_to_source_images,
-    radio_to_image,
-)
-from spf.rf import (
-    ULADetector,
-    phase_diff_to_theta,
-    pi_norm,
-    precompute_steering_vectors,
-    segment_session,
-    segment_session_star,
-    speed_of_light,
-    torch_get_phase_diff,
-    torch_pi_norm,
-)
+    v2_avg_phase_diff_idxs, v2_beamformer_start_idx, v2_column_names,
+    v2_gain_idxs, v2_rssi_idxs, v2_rx_pos_idxs, v2_rx_theta_idx, v2_time_idx,
+    v2_tx_pos_idxs)
+from spf.plot.image_utils import (detector_positions_to_theta_grid,
+                                  labels_to_source_images, radio_to_image)
+from spf.rf import (ULADetector, phase_diff_to_theta, pi_norm,
+                    precompute_steering_vectors, segment_session,
+                    segment_session_star, speed_of_light, torch_get_phase_diff,
+                    torch_pi_norm)
 from spf.sdrpluto.sdr_controller import rx_config_from_receiver_yaml
-from spf.utils import (
-    SEGMENTATION_VERSION,
-    new_yarr_dataset,
-    rx_spacing_to_str,
-    to_bin,
-    zarr_open_from_lmdb_store,
-    zarr_shrink,
-)
+from spf.utils import (SEGMENTATION_VERSION, new_yarr_dataset,
+                       rx_spacing_to_str, to_bin, zarr_open_from_lmdb_store,
+                       zarr_shrink)
 
 
 # from Stackoverflow
--- spf/scripts/segment_zarr.py
+++ spf/scripts/segment_zarr.py
@@ -7,6 +7,9 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input-zarr", type=str, help="input zarr")
     parser.add_argument("-c", "--precompute-cache", type=str, help="precompute cache")
     parser.add_argument("--gpu", default=True, action=argparse.BooleanOptionalAction)
+    parser.add_argument(
+        "-p", "--parallel", type=int, default=24, help="precompute cache"
+    )
     args = parser.parse_args()
 
     ds = v5spfdataset(
@@ -16,5 +19,7 @@ if __name__ == "__main__":
         gpu=args.gpu,
         skip_fields=set(["signal_matrix"]),
         ignore_qc=True,
+        # readahead=True, #this is hard coded in the segmentation code
+        n_parallel=args.parallel,
     )
     print(args.input_zarr, ds.phi_drifts[0], ds.phi_drifts[1])
