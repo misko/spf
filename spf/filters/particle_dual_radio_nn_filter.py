47dde417ff5038f7c5cfc2ac5789025d0276b0e5
misko <misko@cs.toronto.edu>
2024-11-30 19:50:34 -0800

    fix quotes ; scripts update

--- spf/filters/particle_dual_radio_nn_filter.py
+++ spf/filters/particle_dual_radio_nn_filter.py
@@ -32,7 +32,7 @@ class PFSingleThetaDualRadioNN(ParticleFilter):
         assert (
             self.ds.empirical_data_fn
             == checkpoint_config["datasets"]["empirical_data_fn"]
-        ), f"Empirical pkl path from ds does not match that from checkpoint {self.ds.empirical_data_fn} vs {checkpoint_config["datasets"]["empirical_data_fn"]}"
+        ), f"Empirical pkl path from ds does not match that from checkpoint {self.ds.empirical_data_fn} vs {checkpoint_config['datasets']['empirical_data_fn']}"
 
         if not self.ds.temp_file:
             # cache model results
--- spf/scripts/run_filters.sh
+++ spf/scripts/run_filters.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-val_file=/mnt/4tb_ssd/nosig_data/nov23_val.txt
-root=/home/mouse9911/gits/spf/
-precompute_cache=/mnt/4tb_ssd/precompute_cache_new
-input_zarrs=/mnt/4tb_ssd/nosig_data/wallarrayv3_2024_*.zarr
+#val_file=/mnt/4tb_ssd/nosig_data/nov23_val.txt
+
+
+
+val_file=/mnt/4tb_ssd/nosig_data/val.txt
 root=/home/mouse9911/gits/spf
-inference_cache=/mnt/4tb_ssd/inference_cache/ 
 
 shuf --random-source ${val_file} ${val_file} | while read x; do 
-   python ${root}/spf/scripts/run_filters_on_data.py -d $x --nthetas 65 --device cpu --skip-qc  \
-	   --precompute-cache ${precompute_cache} --empirical-pkl-fn ${root}/empirical_dists/full.pkl \
-	   --parallel 32 --work-dir ${root}/spf/run_on_filters_nov27 --config ${root}/spf/model_training_and_inference/models/ekf_and_pf_config.yml
+   echo python ${root}/spf/scripts/run_filters_on_data.py -d $x --nthetas 65 --device cpu --skip-qc  \
+	--empirical-pkl-fn ${root}/empirical_dists/full.pkl \
+	--parallel 24  --work-dir ${root}/spf/run_on_filters_nov30 --config ${root}/spf/model_training_and_inference/models/ekf_and_pf_config.yml
 done
