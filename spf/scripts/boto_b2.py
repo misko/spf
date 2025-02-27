bd28962ee58812ff83bff00876e62ba6c092983f
Ubuntu <ubuntu@ip-172-31-29-243.us-west-1.compute.internal>
2025-02-27 16:18:42 +0000

    updates

--- spf/scripts/boto_b2.py
+++ spf/scripts/boto_b2.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
 
-# -*-coding:utf-8 -*-
-
-""" A python script for working with Backblaze B2 """
-
-""" More Instructions here:  http://www.backblaze.com/b2/docs/s3/python.html """
-""" Video Code Walkthroughs - A playlist for related videos is here:   
-        https://www.youtube.com/c/backblaze/playlists  """
-""" This source code on Github here:  
-    https://github.com/backblaze-b2-samples/b2-python-s3-sample/ """
-""" Sample data in *PUBLIC* bucket here:  
-    https://s3.us-west-002.backblazeb2.com/developer-b2-quick-start/album/photos.html """
-
 import boto3  # REQUIRED! - Details here: https://pypi.org/project/boto3/
 from botocore.exceptions import ClientError
 from botocore.config import Config
@@ -19,84 +7,22 @@ from dotenv import load_dotenv  # Project Must install Python Package:  python-d
 import os
 import sys
 
-WEEK_IN_SECONDS = 604800
+from spf.utils import get_md5_of_file
 
-# Copy the specified existing object in a B2 bucket, creating a new copy in a second B2 bucket
-def copy_file(source_bucket, destination_bucket, source_key, destination_key, b2):
-    try:
-        source = {
-            'Bucket': source_bucket,
-            'Key': source_key
-        }
-        b2.Bucket(destination_bucket).copy(source, destination_key)
-    except ClientError as ce:
-        print('error', ce)
-
-
-# Create the specified bucket on B2
-def create_bucket(name, b2, secure=False):
-    try:
-        b2.create_bucket(Bucket=name)
-        if secure:
-            prevent_public_access(name, b2)
-    except ClientError as ce:
-        print('error', ce)
-
-
-# Delete the specified bucket from B2
-def delete_bucket(bucket, b2):
-    try:
-        b2.Bucket(bucket).delete()
-    except ClientError as ce:
-        print('error', ce)
-
-
-# Delete the specified objects from B2
-def delete_files(bucket, keys, b2):
-    objects = []
-    for key in keys:
-        objects.append({'Key': key})
-    try:
-        b2.Bucket(bucket).delete_objects(Delete={'Objects': objects})
-    except ClientError as ce:
-        print('error', ce)
-
-
-# Delete the specified object from B2 - all versions
-def delete_files_all_versions(bucket, keys, client):
-    objects = []
-    for key in keys:
-        objects.append({'Key': key})
-    try:
-        # SOURCE re LOGIC FOLLOWING:  https://stackoverflow.com/questions/46819590/delete-all-versions-of-an-object-in-s3-using-python
-        paginator = client.get_paginator('list_object_versions')
-        response_iterator = paginator.paginate(Bucket=bucket)
-        for response in response_iterator:
-            versions = response.get('Versions', [])
-            versions.extend(response.get('DeleteMarkers', []))
-            for version_id in [x['VersionId'] for x in versions
-                               if x['Key'] == key and x['VersionId'] != 'null']:
-                print('Deleting {} version {}'.format(key, version_id))
-                client.delete_object(Bucket=bucket, Key=key, VersionId=version_id)
-
-    except ClientError as ce:
-        print('error', ce)
-
-# Download the specified object from B2 and write to local file system
-def download_file(bucket, directory, local_name, key_name, b2):
-    file_path = directory + '/' + local_name
-    try:
-        b2.Bucket(bucket).download_file(key_name, file_path)
-    except ClientError as ce:
-        print('error', ce)
-
+os.environ["AWS_REQUEST_CHECKSUM_CALCULATION"] = "when_required" 
+os.environ["AWS_RESPONSE_CHECKSUM_VALIDATION"] = "when_required"
 
 # Return a boto3 client object for B2 service
 def get_b2_client(endpoint, key_id, application_key):
         b2_client = boto3.client(service_name='s3',
                                  endpoint_url=endpoint,                # Backblaze endpoint
                                  aws_access_key_id=key_id,              # Backblaze keyID
-                                 aws_secret_access_key=application_key) # Backblaze applicationKey
+                                 aws_secret_access_key=application_key, # Backblaze applicationKey
+                                 config=Config(
+                                        s3={
+                                            'checksum_mode': 'disabled',
+                                        }
+                                    ))
         return b2_client
 
 
@@ -108,68 +34,11 @@ def get_b2_resource(endpoint, key_id, application_key):
                         aws_secret_access_key=application_key, # Backblaze applicationKey
                         config = Config(
                             signature_version='s3v4',
+                            s3={
+                                            'checksum_mode': 'disabled',
+                                        }
                     ))
     return b2
-
-
-# Return presigned URL of the object in the specified bucket - Useful for *PRIVATE* buckets
-def get_object_presigned_url(bucket, key, expiration_seconds, b2):
-    try:
-        response = b2.meta.client.generate_presigned_url(ClientMethod='get_object',
-                                                         ExpiresIn=expiration_seconds,
-                                                         Params={
-                                                                    'Bucket': bucket,
-                                                                    'Key': key
-                                                                } )
-        return response
-
-    except ClientError as ce:
-        print('error', ce)
-
-
-# List the buckets in account in the specified region
-def list_buckets(b2_client, raw_object=False):
-    try:
-        my_buckets_response = b2_client.list_buckets()
-
-        print('\nBUCKETS')
-        for bucket_object in my_buckets_response[ 'Buckets' ]:
-            print(bucket_object[ 'Name' ])
-
-        if raw_object:
-            print('\nFULL RAW RESPONSE:')
-            print(my_buckets_response)
-
-    except ClientError as ce:
-        print('error', ce)
-
-# List the keys of the objects in the specified bucket
-def list_object_keys(bucket, b2):
-    try:
-        response = b2.Bucket(bucket).objects.all()
-
-        return_list = []               # create empty list
-        for object in response:        # iterate over response
-            return_list.append(object.key) # for each item in response append object.key to list
-        return return_list             # return list of keys from response
-
-    except ClientError as ce:
-        print('error', ce)
-
-
-# List browsable URLs of the objects in the specified bucket - Useful for *PUBLIC* buckets
-def list_objects_browsable_url(bucket, endpoint, b2):
-    try:
-        bucket_object_keys = list_object_keys(bucket, b2)
-
-        return_list = []                # create empty list
-        for key in bucket_object_keys:  # iterate bucket_objects
-            url = "%s/%s/%s" % (endpoint, bucket, key) # format and concatenate strings as valid url
-            return_list.append(url)     # for each item in bucket_objects append value of 'url' to list
-        return return_list              # return list of keys from response
-
-    except ClientError as ce:
-        print('error', ce)
 
 
 # Upload specified file into the specified bucket
@@ -185,216 +54,61 @@ def upload_file(bucket, directory, file, b2, b2path=None):
 
     return response
 
+def download_folder(b2_client, bucket, prefix, local_dir):
+    os.makedirs(local_dir)
+    resp = b2_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    for obj in resp.get('Contents', []):
+        filename = obj['Key']
+        local_filename=f'{local_dir}/{os.path.basename(filename)}'
+        print(f"Downloading {filename} -> {local_filename}")
+        b2_client.download_file(bucket, filename, local_filename)
 
-"""
-Python main() 
-
-Basic execution setup
-Then conditional blocks executing based on command-line arguments passed as input.
-"""
 def main():
-    #args = sys.argv[1:]  # retrieve command-line arguments passed to the script
+    load_dotenv()
+    endpoint_rw = os.getenv("ENDPOINT_URL_YOUR_BUCKET")
+    key_id_rw = os.getenv("KEY_ID_YOUR_ACCOUNT")
+    application_key_rw = os.getenv("APPLICATION_KEY_YOUR_ACCOUNT")
 
-    load_dotenv()   # load environment variables from file .env
-
-    endpoint_rw = os.getenv("ENDPOINT_URL_YOUR_BUCKET")  # getting environment variables from file .env
-    key_id_rw = os.getenv("KEY_ID_YOUR_ACCOUNT")  # getting environment variables from file .env
-    application_key_rw = os.getenv("APPLICATION_KEY_YOUR_ACCOUNT")  # getting environment variables from file .env
-    b2_rw = get_b2_resource(endpoint_rw, key_id_rw, application_key_rw)
     b2_client = get_b2_client(endpoint_rw, key_id_rw, application_key_rw)
 
-    b2_client = b2_rw.meta.client
-    r=b2_client.list_objects_v2(
-        Bucket='projectspf',
-        Prefix=''
-    )
-    print(r)
-    sys.exit(1)
-
-    r=list_object_keys('projectspf',b2_rw)
-    print(r)
-    sys.exit(1)
+    bucket = 'projectspf'
     
-    # Call function to return reference to B2 service
-    b2 = get_b2_resource(endpoint, key_id_ro, application_key_ro)
+    #first need to download the model and get checksums
+    inference_model_prefix='md2/cache/inference_models'+'/paired_checkpoints_windowedbeamformer_nosig_3p5_randx_wd0p02_gains_vehicle_0p2dropout_noroverbounce'
+    local_model_folder='./model'
+    download_folder(b2_client,bucket,inference_model_prefix,local_model_folder)
 
-    # Call function to return reference to B2 service
-    b2_client = get_b2_client(endpoint, key_id_ro, application_key_ro)
+    model_config_fn=f'{local_model_folder}/config.yml'
+    model_checkpoint_fn=f'{local_model_folder}/best.pth'
+    config_checksum = get_md5_of_file(model_config_fn)
+    checkpoint_checksum = get_md5_of_file(model_checkpoint_fn)
 
-    # get environment variables from file .env
-    key_id_private_ro = os.getenv("KEY_ID_PRIVATE_RO")  # Backblaze keyID
-    application_key_private_ro = os.getenv("APPLICATION_KEY_PRIVATE_RO") # Backblaze applicationKey
+    print(config_checksum,checkpoint_checksum)
 
-    # Call function to return reference to B2 service using a second set of keys
-    b2_private = get_b2_resource(endpoint, key_id_private_ro, application_key_private_ro)
+    #download inference cache /mnt/md2$ ls cache/inference/dec18_mission1_rover1.zarr/3.500/dc0661eb09c048996e81545363ff8e33/d1655af080f3721a7e4852221955950e.npz 
 
-    # WRITE OPERATIONS - THIS BLOCK CREATES B2 OBJECTS THAT SUPPORT WRITE OPERATIONS FOR BUCKETS IN YOUR ACCOUNT
-    if len(args) == 1 and args[0] >= '20':
-        endpoint_rw = os.getenv("ENDPOINT_URL_YOUR_BUCKET")  # getting environment variables from file .env
-        key_id_rw = os.getenv("KEY_ID_YOUR_ACCOUNT")  # getting environment variables from file .env
-        application_key_rw = os.getenv("APPLICATION_KEY_YOUR_ACCOUNT")  # getting environment variables from file .env
-        # Call function to return reference to B2 service
-        b2_rw = get_b2_resource(endpoint_rw, key_id_rw, application_key_rw)
+    prefix = 'md2/cache/nosig_data'
 
-        # Call function to return reference to B2 service
-        b2_client_rw = get_b2_client(endpoint_rw, key_id_rw, application_key_rw)
+    # Use the *custom* client for listing and downloading:
+    resp = b2_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    for obj in resp.get('Contents', []):
+        filename = obj['Key']
+        if '.yaml' in filename :
+            remote_zarr_fn=filename.replace('.yaml','.zarr')
+            base_zarr_fn=os.path.basename(remote_zarr_fn)
+            print(remote_zarr_fn,filename)
 
-    # BEGIN if elif BLOCKS FOR EACH PARAM
-    # 01 - list_objects
-    if len(args) == 0 or (len(args) == 1 and args[0] == '01'):
-        # Call function to return list of object 'keys'
-        bucket_object_keys = list_object_keys(PUBLIC_BUCKET_NAME, b2)
-        for key in bucket_object_keys:
-            print(key)
-
-        print('\nBUCKET ', PUBLIC_BUCKET_NAME, ' CONTAINS ', len(bucket_object_keys), ' OBJECTS')
-
-    # 02 - List Objects formatted as browsable url
-    # IF *PUBLIC* BUCKET, PRINT OUTPUTS BROWSABLE URL FOR EACH FILE IN THE BUCKET
-    elif len(args) == 1 and ( args[0] == '02' or args[0] == '02PUB' ):
-        # Call function to return list of object 'keys' formatted into friendly urls
-        browsable_urls = list_objects_browsable_url(PUBLIC_BUCKET_NAME, endpoint, b2)
-        for url in browsable_urls:
-            print(url)
-
-        print('\nBUCKET ', PUBLIC_BUCKET_NAME, ' CONTAINS ', len(browsable_urls), ' OBJECTS')
+            # download the nosig data
+            download_folder(b2_client,bucket,remote_zarr_fn,base_zarr_fn)
+            # download the inference cache
 
 
-    # IF *PRIVATE* BUCKET, PRINT OUTPUTS URL THAT WHEN USED IN BROWSER RETURNS ERROR: UnauthorizedAccess
-    elif len(args) == 1 and args[0] == '02PRI':
-        # Call function to return list of object 'keys' concatenated into friendly urls
-        browsable_urls = list_objects_browsable_url(PRIVATE_BUCKET_NAME, endpoint, b2_private)
-        for key in browsable_urls:
-            print(key)
+            #print(f"Downloading {filename} -> {base_filename}")
+            # Use that same custom client
+            #b2_client.download_file(bucket, filename, base_filename)
+            break
 
-        print('\nBUCKET ', PRIVATE_BUCKET_NAME, ' CONTAINS ', len(browsable_urls), ' FILES')
-
-    # 04 - LIST BUCKETS
-    elif len(args) == 1 and args[0] == '04':
-
-        list_buckets( b2_client, raw_object=True )
-
-
-    # 05 - PRESIGNED URLS
-    elif len(args) == 1 and args[0] == '05':
-        my_bucket = b2_private.Bucket(PRIVATE_BUCKET_NAME)
-        print('my_bucket: ', my_bucket)
-        for my_bucket_object in my_bucket.objects.all():
-            file_url = get_object_presigned_url(PRIVATE_BUCKET_NAME, my_bucket_object.key, 3000, b2_private)
-            print (file_url)
-
-
-    # 06 - DOWNLOAD FILE
-    elif len(args) == 1 and args[0] == '06':
-
-        download_file(bucket = PRIVATE_BUCKET_NAME,
-                      directory = LOCAL_DIR,
-                      local_name = file1_pri,
-                      key_name = file1_pri,
-                      b2 = b2_private)
-
-
-    # 20 - CREATE BUCKET - *SUCCESS*
-    elif len(args) == 1 and args[0] == '20':
-
-        print('BEFORE CREATE NEW BUCKET NAMED:  ',NEW_BUCKET_NAME )
-        list_buckets( b2_client )
-
-        b2 = b2_rw
-
-        #  How To Here:  https://help.backblaze.com/hc/en-us/articles/360047629793-How-to-use-the-AWS-SDK-for-Python-with-B2-
-        response = b2.create_bucket( Bucket=NEW_BUCKET_NAME )
-
-        print('RESPONSE:  ', response)
-
-        print('\nAFTER CREATE BUCKET')
-        list_buckets( b2_client )
-
-    # 21 - UPLOAD FILE
-    elif len(args) == 1 and args[0] == '21':
-
-        b2 = b2_rw
-
-        response = upload_file(NEW_BUCKET_NAME, LOCAL_DIR, file1, b2)
-
-        print('RESPONSE:  ', response)
-
-        generate_friendly_url( NEW_BUCKET_NAME, endpoint, b2)
-
-
-    # 22 - Copy File Between Buckets
-    elif len(args) == 1 and args[0] == '22':
-
-        b2 = b2_rw
-
-        response = b2.create_bucket( Bucket=TRANSIENT_BUCKET_NAME )
-
-        list_buckets( b2_client )
-
-        print('\nBEFORE CONTENTS BUCKET ', TRANSIENT_BUCKET_NAME)
-        copy_file(NEW_BUCKET_NAME, TRANSIENT_BUCKET_NAME, file1, file1, b2)
-
-        print('\nAFTER CONTENTS BUCKET ', TRANSIENT_BUCKET_NAME)
-        my_bucket = b2.Bucket(TRANSIENT_BUCKET_NAME)
-        for my_bucket_object in my_bucket.objects.all():
-            print(my_bucket_object.key)
-
-    # 30 - Delete File
-    elif len(args) == 1 and args[0] == '30':
-        print('BEFORE - Bucket Contents ')
-        my_bucket = b2.Bucket(NEW_BUCKET_NAME)
-        for my_bucket_object in my_bucket.objects.all():
-            print(my_bucket_object.key)
-
-        print('File Name to Delete:  ', file1)
-
-        b2 = b2_rw
-
-        delete_files(NEW_BUCKET_NAME, [file1], b2)
-
-        print('\nAFTER - Bucket Contents ')
-        my_bucket = b2.Bucket(NEW_BUCKET_NAME)
-        for my_bucket_object in my_bucket.objects.all():
-            print(my_bucket_object.key)
-
-    # 31 - Delete File all versions
-    elif len(args) == 1 and args[0] == '31':
-        print('BEFORE - Bucket Contents ')
-        b2 = b2_rw
-        my_bucket = b2.Bucket(NEW_BUCKET_NAME)
-        for my_bucket_object in my_bucket.objects.all():
-            print(my_bucket_object.key)
-
-        print('File Name to Delete:  ', file1)
-
-        b2_client = b2_client_rw
-
-        delete_files_all_versions(NEW_BUCKET_NAME,
-                                  [file1],
-                                  b2_client )
-
-        print('\nAFTER - Bucket Contents ')
-        my_bucket = b2.Bucket(NEW_BUCKET_NAME)
-        for my_bucket_object in my_bucket.objects.all():
-            print(my_bucket_object.key)
-
-
-    # Cannot delete non-empty bucket - error An error occurred (BucketNotEmpty) when calling the DeleteBucket operation:
-    # 32 - Delete Bucket
-    elif len(args) == 1 and args[0] == '32':
-
-        print('BEFORE - Buckets ')
-        list_buckets( b2_client )
-
-        print('Bucket Name to Delete:  ', NEW_BUCKET_NAME)
-
-        b2 = b2_rw
-
-        delete_bucket(NEW_BUCKET_NAME, b2)
-
-        print('\nAFTER - Buckets ')
-        list_buckets( b2_client )
-
+        
 
 # Optional (not strictly required)
 if __name__ == '__main__':
